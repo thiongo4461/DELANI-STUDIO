f73a5120b159b4ae04c7221b18d53e4337c48c09
yellowB <rosemarythiongo2000@gmail.com>
2022-01-17 19:18:50 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+$('.one').click(function(evt){
+    var currentID = this.id || "No ID!";
+    var element = document.getElementById("dis-one");
+    element.classList.toggle("invisible");
+    console.log(`this is the id ${element}`)
+  });
+
+$('.two').click(function(evt){
+    var currentID = this.id || "No ID!";
+    var element = document.getElementById("dis-two");
+    element.classList.toggle("invisible");
+    console.log(`this is the id ${element}`)
+  });
+
+$('.three').click(function(evt){
+    var currentID = this.id || "No ID!";
+    var element = document.getElementById("dis-three");
+    element.classList.toggle("invisible");
+    console.log(`this is the id ${element}`)
+  });
